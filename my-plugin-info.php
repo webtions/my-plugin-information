6feb742f2a8c88721e36639bae7bdd47a432a196
Harish Chouhan <me@harishchouhan.com>
2025-07-26 22:31:56 +0700

    Update plugin header to match current standards and branding

--- my-plugin-info.php
+++ my-plugin-info.php
@@ -1,30 +1,21 @@
 <?php
-/*
-Plugin Name: My Plugin Information
-Plugin URI: http://themeist.co/plugins/wordpress/my-plugin-information/
-Description: Communicate with WordPress.org Plugins API to retrive your Plugin Information
-Version: 0.3
-Author: Harish Chouhan, Themeist.co
-Author URI: http://themeist.co
-License: GPL v3
-
-MailChimp for WordPress
-Copyright (C) 2013-2015, Themeist, hello@dreamsmedia.in
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Plugin Name:       My Plugin Information
+ * Plugin URI:        https://themeist.com/plugins/wordpress/my-plugin-information/#utm_source=wp-plugin&utm_medium=my-plugin-information&utm_campaign=plugins-page
+ * Description:       Developer-focused plugin to fetch and display information from the WordPress.org Plugin API.
+ * Version:           0.4
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            Themeist
+ * Author URI:        https://themeist.com/
+ * Author Email:      support@themeist.com
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       my-plugin-information
+ * Domain Path:       /languages
+ *
+ * @package MyPluginInformation
+ */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
